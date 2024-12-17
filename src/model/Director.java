575242c262079594d805015a112183213c5c19a8
I.Maksat <maksat@Air-Maksat.lan>
2024-12-17 11:19:58 -0800

    Storing information about the director

---
+++ src/model/Director.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Director {
+    private String fullName;
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+}
