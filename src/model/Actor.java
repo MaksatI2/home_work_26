54b73101b433afde6b2307b8a9ca8e0c7012b642
I.Maksat <maksat@Air-Maksat.lan>
2024-12-17 11:18:43 -0800

    Contains information about the actor and his role

---
+++ src/model/Actor.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Actor {
+    private String fullName;
+    private String role;
+
+    public String getFullName() { return fullName; }
+    public String getRole() { return role; }
+
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setRole(String role) { this.role = role; }
+}
